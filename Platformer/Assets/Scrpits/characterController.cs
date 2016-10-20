bbf4b37d788c02ca4ef202b211aecd2ece6d6574
asus\1004998 <2>
2016-10-20 10:09:37 +0500

    no message

--- Platformer/Assets/Scrpits/characterController.cs
+++ Platformer/Assets/Scrpits/characterController.cs
@@ -1,83 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class characterController : MonoBehaviour {
-	public float maxSpeed = 10f;
-	public float jumpForce = 700f;
-	bool facingRight = true;
-	bool grounded = false;
-	public Transform groundCheck;
-	public float groundRadius = 0.2f;
-	public LayerMask whatIsGround;
-	public float score;
-	public float move;
+public class characterController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    public float jumpForce = 700f;
+    bool facingRight = true;
+    bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float score;
+    public float move;
 
-	private GameObject star;
-	// Use this for initialization
-	void Start () {
+    private GameObject star;
+    // Use this for initialization
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
 
-
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-
-		move = Input.GetAxis ("Horizontal");
-
-	}
-
-	void Update(){
-		if (grounded && (Input.GetKeyDown (KeyCode.W)||Input.GetKeyDown (KeyCode.UpArrow))) {
-
-			GetComponent<Rigidbody2D>().AddForce (new Vector2(0f,jumpForce));
-		}
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		
-		if (move > 0 && !facingRight)
-			Flip ();
-		else if (move < 0 && facingRight)
-			Flip ();
+    // Update is called once per frame
+    void FixedUpdate()
+    {
 
 
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 
-		if (Input.GetKey(KeyCode.Escape))
-		{
-			Application.Quit();
-		}
+        move = Input.GetAxis("Horizontal");
 
-		if (Input.GetKey(KeyCode.R))
-		{
-			Application.LoadLevel(Application.loadedLevel);
-		}
+    }
+
+    void Update()
+    {
+        if (grounded && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
 
 
-	}
-	
-	void Flip(){
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
 
-	void OnTriggerEnter2D(Collider2D col){
-		if ((col.gameObject.name == "dieCollider")||(col.gameObject.name == "saw"))
-						Application.LoadLevel (Application.loadedLevel);
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 
-	    if (/*col.gameObject.name == "star"*/col.gameObject.name.Contains("star")) {
-						score++;
-						Destroy (col.gameObject);
-				}
+        if (Input.GetKey(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
 
-		if (col.gameObject.name == "endLevel") {
-			if (!(GameObject.Find("star"))) Application.LoadLevel ("scene2");
-				}
-	}
 
-	void OnGUI(){
-				GUI.Box (new Rect (0, 0, 100, 100), "Stars: " + score);
-		}
-		
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if ((col.gameObject.name == "dieCollider") || (col.gameObject.name == "saw"))
+            Application.LoadLevel(Application.loadedLevel);
+
+        if (/*col.gameObject.name == "star"*/col.gameObject.name.Contains("star"))
+        {
+            score++;
+            Destroy(col.gameObject);
+        }
+
+        if (col.gameObject.name == "endLevel")
+        {
+            if (!(GameObject.Find("star"))) Application.LoadLevel("scene2");
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 100), "Stars: " + score);
+    }
+
 }
