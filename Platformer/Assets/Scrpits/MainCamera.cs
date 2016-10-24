74033da39b8b070f5e5c77017a721950688ef833
asus\1004998 <2>
2016-10-24 11:47:58 +0500

    fix background

--- Platformer/Assets/Scrpits/MainCamera.cs
+++ Platformer/Assets/Scrpits/MainCamera.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class MainCamera : MonoBehaviour {
-
+public class MainCamera : MonoBehaviour
+{
     public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
     public Transform target;
+    public Transform background;
 
-    // Update is called once per frame
     void Update()
     {
         if (target)
@@ -16,8 +15,8 @@ public class MainCamera : MonoBehaviour {
             Vector3 delta = new Vector3(target.position.x, target.position.y + 0.75f, target.position.z) - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
 
-
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            background.position = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 20));
         }
 
     }
