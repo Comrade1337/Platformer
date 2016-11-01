e7727052252d09c4578f7229bcf3855cfaaaad46
asus\1004998 <2>
2016-11-01 12:16:46 +0500

    moving platform

---
+++ Platformer/Assets/Scrpits/MovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float delta;
+    public float speed = 0.05f;
+
+    Vector3 _startPosition;
+    float _finishPosition;
+    bool movingUp;
+
+    void Awake()
+    {
+        _startPosition = transform.position;
+        _finishPosition = _startPosition.y + delta;
+    }
+    
+    void FixedUpdate()
+    {
+        if (transform.position.y >= _finishPosition)
+            movingUp = false;
+        if (transform.position.y <= _startPosition.y)
+            movingUp = true;
+
+        transform.position += movingUp ? new Vector3(0, speed, 0) : new Vector3(0, -speed, 0);
+    }
+}
