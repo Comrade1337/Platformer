fbdabfc8e68e57cc3e7ed029caf281804237bd0a
asus\1004998 <2>
2016-11-01 13:54:34 +0500

    moving flatform

---
+++ Platformer/Assets/Scrpits/MovingSaw.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingSaw : MonoBehaviour {
+    public float delta;
+    public float speed = 0.05f;
+    public bool movingOnX;
+    public bool movingOnY;
+
+    Vector3 _startPosition;
+    Vector3 _finishPosition;
+    bool movingUp;
+
+    void Awake()
+    {
+        _startPosition = transform.position;
+        _finishPosition.y = _startPosition.y + delta;
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.y >= _finishPosition.y)
+            movingUp = false;
+        if (transform.position.y <= _startPosition.y)
+            movingUp = true;
+
+        transform.position += movingUp && _startPosition.y < _finishPosition.y ? new Vector3(0, speed, 0) : new Vector3(0, -speed, 0);
+    }
+}
