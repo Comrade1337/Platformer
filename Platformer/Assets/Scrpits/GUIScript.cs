13d7e63caa651e09bc934e1454212cd7fefa967b
asus\1004998 <2>
2016-11-02 14:14:49 +0500

    level 5

--- Platformer/Assets/Scrpits/GUIScript.cs
+++ Platformer/Assets/Scrpits/GUIScript.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class GUIScript : MonoBehaviour {
     public Font font;
-    
+    public Color color;
+
     GUIStyle guiStyle = new GUIStyle();
 
     void Awake()
@@ -14,7 +14,10 @@ public class GUIScript : MonoBehaviour {
 
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 100, 100), "Score: " + Data.Score + "\n\nStars: " + Data.Stars + "\nLives: " + Data.Lives, guiStyle);
+        GUI.Label(new Rect(10, 10, 100, 100), 
+            "Score: " + Data.Score + 
+            "\n\nStars: " + Data.Stars + 
+            "\nLives: " + Data.Lives, guiStyle);
     }
 
 }
