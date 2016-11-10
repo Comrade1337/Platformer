b513f7be58b904bf485e5021f99c49e72a6c54e4
asus\1004998 <2>
2016-11-10 12:31:28 +0500

    pause menu

--- Platformer/Assets/Scrpits/PauseGUI.cs
+++ Platformer/Assets/Scrpits/PauseGUI.cs
@@ -1,33 +1,65 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGUI : MonoBehaviour
 {
     public Font font;
 
+    float timer;
+    bool isGuiPause;
     GUIStyle guiTextStyle = new GUIStyle();
-    GUIStyle guiButtonStyle = new GUIStyle();
+    public GUIStyle guiButtonStyle;// = new GUIStyle();
 
     void Awake()
     {
         guiTextStyle.font = font;
         guiTextStyle.fontSize = 50;
-        guiTextStyle.normal.textColor = new Color(0.4F, 0.4F, 0.4F);
-        guiTextStyle.hover.textColor = new Color(1F, 1F, 1F);
-
+        guiTextStyle.normal.textColor = new Color(0.2F, 0.2F, 0.2F);
+                
         guiButtonStyle.font = font;
-        guiButtonStyle.fontSize = 40;
-        guiButtonStyle.normal.textColor = new Color(0.4F, 0.4F, 0.4F);
-        guiButtonStyle.hover.textColor = new Color(1F, 1F, 1F);
-        guiButtonStyle.border = new RectOffset(10, 10, 10, 10);
+        guiButtonStyle.fontSize = 45;
     }
 
-    void OnGUI()
+    void Update()
     {
-        GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 100, 100, 100), "[ пауза ]", guiTextStyle);
-
-        if (GUI.Button(new Rect(Screen.width / 2 - 65, Screen.height / 2, 130, 40), "продолжить", guiButtonStyle))
+        Time.timeScale = timer;
+        if (Input.GetKeyDown(KeyCode.Escape) && Data.Paused == false)
             Data.Paused = true;
-        if (GUI.Button(new Rect(Screen.width / 2 - 85, Screen.height / 2 + 50, 180, 40), "выйти из игры", guiButtonStyle))
-            Application.Quit();
+        else if (Input.GetKeyDown(KeyCode.Escape) && Data.Paused == true)
+            Data.Paused = false;
+
+        if (Data.Paused == true)
+        {
+            timer = 0;
+            isGuiPause = true;
+        }
+        else if (Data.Paused == false)
+        {
+            timer = 1f;
+            isGuiPause = false;
+        }
+    }
+    public void OnGUI()
+    {
+        if (isGuiPause == true)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 75f, Screen.height / 2 - 150f, 150f, 45f), "[ пауза ]", guiTextStyle);
+
+            if (GUI.Button(new Rect(Screen.width / 2 - 120f, Screen.height / 2 - 50f, 260f, 45f), "[ продолжить ]", guiButtonStyle))
+            {
+                Data.Paused = false;
+                timer = 0;
+            }
+            if (GUI.Button(new Rect(Screen.width / 2 - 100f, Screen.height / 2, 220f, 45f), "[ сохранить ]", guiButtonStyle))
+            {
+
+            }
+            if (GUI.Button(new Rect(Screen.width / 2 - 80f, Screen.height / 2 + 50, 180f, 45f), "[ в меню ]", guiButtonStyle))
+            {
+                Data.Paused = false;
+                timer = 0;
+                //Application.LoadLevel("Menu");
+            }
+        }
     }
 }
